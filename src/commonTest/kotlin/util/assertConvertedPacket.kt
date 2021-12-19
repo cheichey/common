cfb4338c35b234761d1a80372444c6fed859877d
cheichey <cheichey.1223@gmail.com>
2021-12-19 12:53:43 +0900

    fix: typo を修正

--- src/commonTest/kotlin/util/assertConvertedPacket.kt
+++ src/commonTest/kotlin/util/assertConvertedPacket.kt
@@ -10,11 +10,11 @@ import kotlin.test.assertNotNull
 inline fun <reified T : PacketData> assertConvertedPacket(packetType: PacketType<T>, data: T, size: Int) {
     val bytes = Packet.toByteArray(packetType, data)
     assertEquals(size, bytes.size)
-    val rowPacket = Packet.fromByteArray(bytes)
-    assertNotNull(rowPacket)
-    assertEquals(packetType.id, rowPacket.id)
-    assertEquals(size - 2, rowPacket.array.size)
-    val packet = rowPacket.toPacket(packetType)
+    val rawPacket = Packet.fromByteArray(bytes)
+    assertNotNull(rawPacket)
+    assertEquals(packetType.id, rawPacket.id)
+    assertEquals(size - 2, rawPacket.array.size)
+    val packet = rawPacket.toPacket(packetType)
     assertNotNull(packet)
     assertEquals(packetType.id, packet.type.id)
     val packetData = packet.data
